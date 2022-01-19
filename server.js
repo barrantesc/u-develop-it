7cdc49635b83bf5d577a0c6f8bbc4c87fae43be7
playground <connie.s.barrantes@gmail.com>
2022-01-19 18:01:17 -0500

    created a candidate

--- server.js
+++ server.js
@@ -21,15 +21,47 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+// //Get all candidates
+//   db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//     });
 
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-    });
+// // Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// //Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+// the ? is a placeholder per each value
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
   });
+
+
 
 //This function will start express.js on port 3001
 app.listen(PORT, () => {
